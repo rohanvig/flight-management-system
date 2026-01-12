b3d8387f0951c4265aa60047437b7882cba8f9fc
Hartik-spark <rohankumarldttechnology@gmail.com>
2026-01-12 15:56:39 +0530

    remove some extra thngs and fix the configuration files of ts , also add the package required in he user-service for postgres

--- apps/user-service/src/repository/user.repo.ts
+++ apps/user-service/src/repository/user.repo.ts
@@ -1,15 +1,21 @@
-import { UserModel, IUser } from "../model/user.model.js";
+import { User } from "../model/user.model.js";
+import type {
+  UserAttributes,
+  UserCreationAttributes,
+} from "../model/user.model.js";
 
 export class UserRepository {
-  async findByEmail(email: string): Promise<IUser | null> {
-    return await UserModel.findOne({ email });
+  async findByEmail(email: string): Promise<User | null> {
+    return await User.findOne({
+      where: { email },
+    });
   }
 
-  async findById(id: string): Promise<IUser | null> {
-    return await UserModel.findById(id);
+  async findById(id: string): Promise<User | null> {
+    return await User.findByPk(id);
   }
 
-  async create(userData: Omit<IUser, "_id" | "createdAt">): Promise<IUser> {
-    return await UserModel.create(userData);
+  async create(userData: UserCreationAttributes): Promise<User> {
+    return await User.create(userData);
   }
 }
